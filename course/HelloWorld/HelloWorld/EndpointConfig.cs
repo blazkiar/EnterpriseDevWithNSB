b1a88537b0d7f25040f0dd5b1d5f8329f0065d60
Jimmy P <mail@thejimmyp.com>
2012-07-31 08:16:02 +1000

    complete exercise 1

---
+++ course/HelloWorld/HelloWorld/EndpointConfig.cs
@@ -0,0 +1,17 @@
+﻿using NServiceBus;
+using log4net;
+
+namespace HelloWorld
+{
+    public class EndpointConfig : IConfigureThisEndpoint, IWantToRunAtStartup
+    {
+        public void Run()
+        {
+            LogManager.GetLogger(GetType()).Info("Hello World!");
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
