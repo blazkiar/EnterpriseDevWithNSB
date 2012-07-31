783baca96cf034003e83a90986acac0250bd4af6
Jimmy P <mail@thejimmyp.com>
2012-08-01 07:03:07 +1000

    Complete exercise 6

---
+++ course/HelloWorld/HelloWorldServer/EndpointConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace HelloWorldServer
+{
+    public class EndpointConfig: IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            NServiceBus.Configure.With()
+                .DefaultBuilder()
+                .XmlSerializer("http://acme.com/");
+        }
+    }
+}
---
+++ course/HelloWorld/HelloWorldServer/RequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Messages;
+using NServiceBus;
+using log4net;
+
+namespace HelloWorldServer
+{
+    class RequestHandler : IHandleMessages<Request>
+    {
+        public void Handle(Request message)
+        {
+            LogManager.GetLogger("RequestHandler").Info(message.SaySomething);
+        }
+    }
+
+}
