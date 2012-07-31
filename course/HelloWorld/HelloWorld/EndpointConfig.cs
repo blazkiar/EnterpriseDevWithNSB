8d3d165c28c45d106cf21c83ffd0c177a9ff83f9
Jimmy P <mail@thejimmyp.com>
2012-08-01 06:43:23 +1000

    Complete excercise 4

--- course/HelloWorld/HelloWorld/EndpointConfig.cs
+++ course/HelloWorld/HelloWorld/EndpointConfig.cs
@@ -3,5 +3,13 @@ using log4net;
 
 namespace HelloWorld
 {
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client {}
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            NServiceBus.Configure.With()
+                .DefaultBuilder()
+                .XmlSerializer("http://acme.com/");
+        }
+    }
 }
