4b723044d328077667488bba3e37f6c0935814fa
Jimmy P <mail@thejimmyp.com>
2012-08-02 21:42:10 +1000

    Complete exercise 15

---
+++ course/HelloWorld/HelloWorld.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Messages;
+using NServiceBus.Testing;
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestRequestHandler()
+        {
+            Test.Initialize();
+
+            Test.Handler<RequestWithResponseHandler>()
+                .ExpectReturn<int>(i => i ==0)
+                .OnMessage<RequestWithResponse>(m => m.SaySomething = "");
+        }
+    }
+}
