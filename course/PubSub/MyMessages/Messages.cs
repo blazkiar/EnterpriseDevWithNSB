c854d933da7aae43188020bc406a8035bad8c4ce
Jimmy P <mail@thejimmyp.com>
2012-08-04 10:47:05 +1000

    Complete pub sub exercise

---
+++ course/PubSub/MyMessages/Messages.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyMessages
+{
+    [Serializable]
+    public class EventMessage : IMyEvent
+    {
+        public Guid EventId { get; set; }
+        public DateTime? Time { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+
+    public interface IMyEvent
+    {
+        Guid EventId { get; set; }
+        DateTime? Time { get; set; }
+        TimeSpan Duration { get; set; }
+    }
+}
